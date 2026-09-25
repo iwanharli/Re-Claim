bb374cb94d92f60de249e117835e852943ff430a
Keane Emanuel Dalisay <keandals21@gmail.com>
2026-09-25 15:43:15 +0800

    Cut unnecessary Google Font links

--- src/Views/errors/404.php
+++ src/Views/errors/404.php
@@ -6,20 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Page Not Found | ReClaim</title>
     <link rel="stylesheet" href="/css/app.css">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link
-        href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&family=Outfit:wght@400;600;800&display=swap"
-        rel="stylesheet">
     <style>
-        body {
-            font-family: 'Poppins', sans-serif;
-        }
-
-        .font-outfit {
-            font-family: 'Outfit', sans-serif;
-        }
-
         .animate-float {
             animation: float 6s ease-in-out infinite;
         }
